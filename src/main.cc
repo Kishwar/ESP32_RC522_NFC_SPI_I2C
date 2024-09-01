33a31cc32ba7fba99f650fdba410862cce32a367
Kishwar Kumar <kumar.kishwar@gmail.com>
2024-09-01 12:41:55 +0200

    create project structure

--- src/main.cc
+++ src/main.cc
@@ -1,1 +1,16 @@
-void app_main() {}
+/**
+ * @file main.cc
+ *
+ * @brief Project responsible to read / write operations
+ *        from RFID RC522 (both polling and interrupt)
+ *
+ * @author Kishwar Kumar
+ * Contact: kumar.kishwar@gmail.com
+ * 
+ * @date 01/09/2024
+ *
+ */
+
+extern "C" void app_main() {
+    
+}
